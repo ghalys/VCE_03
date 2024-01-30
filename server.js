e9ae06728bf17154c60715b150ff3170488f0f11
FeliksMarksen <felixmarxen@outlook.com>
2024-01-30 18:32:30 +0100

    Add dependencies and update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+import http from "http";
 import express from "express";
+import ws from "ws";
+
+const WebSocketServer = ws.Server;
+const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ httpServer: server });
+
+var ws = new WebSocket("wss://ecv-etic.upf.edu/node/" + PORT + "/");
+
+PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT} :)`);
+});
