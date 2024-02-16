e6cf3ae8f2efef5072daab4e3414b3483c61681a
ghalys <asentissielidrissi@gmail.com>
2024-02-16 02:43:13 +0100

    drawing is now cleaner

--- public/scripts/world.js
+++ public/scripts/world.js
@@ -1,40 +1,88 @@
 var canvas = document.querySelector("canvas");
+class Position {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
 
-var FACING = {
-  RIGHT : 0,
-  FRONT : 1,
-  LEFT  : 2,
-  BACK  : 3
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static interpolate(start, end, t) {
+    return new Position(
+      start.x + (end.x - start.x) * t,
+      start.y + (end.y - start.y) * t
+    );
+  }
 }
+class Character {
+  static FACING = {
+    RIGHT : 0,
+    FRONT : 1,
+    LEFT  : 2,
+    BACK  : 3
+  }
+  static ANIMATION = {
+    IDLE :[0],
+    TALK : [0,1],
+    SIT : [13],
+    WALK :[2,3,4,5,6,7,8,9]
+  }
+
+  constructor(name, position = new Position(), facing = Character.FACING.FRONT, animation=Character.ANIMATION.TALK) {
+      this.name =name,
+      this.facing = facing,
+      this.position = position,
+      this.animation= animation
+  }
+  //function for changing face direction
+  facingRight(){
+    this.facing = Character.FACING.RIGHT;
+  }
+  facingLeft(){
+    this.facing = Character.FACING.LEFT;
+  }
+  facingFront(){
+    this.facing = Character.FACING.FRONT;
+  }
+  facingBack(){
+    this.facing = Character.FACING.BACK;
+  }
+
+  //function for changing the animation
+  animatIdle(){
+    this.animation = Character.ANIMATION.IDLE;
+  }
+  animatWalk(){
+    this.animation = Character.ANIMATION.WALK;
+  }
+  animatSit(){
+    this.animation = Character.ANIMATION.SIT;
+  }
+  animatTalk(){
+    this.animation = Character.ANIMATION.TALK;
+  }
+
+  moveTo(newX, newY) {
+    this.position.setPosition(newX, newY);
+  }
+  interpolatePosition(endPosition, t) {
+    this.position = Position.interpolate(this.position, endPosition, t);
+  }
+
+}
+//testing
+var avatar = new Character("avatar");
 
 var WORLD ={
-  people :[
-    { name : "javi",
-      facing : FACING.RIGHT,
-      pos:[0,0],
-      animation:"walk"
-    }
-  ]
+  people :
+  [avatar]
 }
 
-var anims = {
-  idle :[0],
-  talk : [0,1],
-  sit : [13],
-  walk :[2,3,4,5,6,7,8,9]
-}
-
+//get image
 var images = {};
-// function getImage(url)
-// {
-//   if(images[url])
-//     return images[url];
-//   var img = new Image();
-//   img.src = url;
-//   images[url] = img;
-//   return img;
-// }
-
 function getImage(url, callback) {
   if (images[url]) {
     callback(images[url]);
@@ -51,26 +99,18 @@ function getImage(url, callback) {
   img.src = url;
 }
 
-// function drawCharacter(ctx, person)
-// {
-//   var img = getImage("Man.png");
-//   ctx.imageSmoothingEnabled = false;
-//   var frame_num = Math.floor((performance.now() / 1000)); 
-//   var anim = anims[ person.animation ];
-//   var frame = anim[ frame_num  % anim.length ];
-//   ctx.drawImage(img,32*frame,64*person.facing,32,64,person.pos[0],person.pos[1],32,64);
-// }
-function drawCharacter(ctx, person) {
+function drawCharacter(ctx, character) {
   getImage("Avatar.png", function(img) {
     ctx.imageSmoothingEnabled = false;
+    
     // Define a speed modifier
-    var speedModifier = 0.5; // Adjust this value to speed up or slow down the animation
+    var speedModifier = 0.5; 
 
     // Adjust the frame calculation to include the speed modifier
-    var frame_num = Math.floor((performance.now() / 100 * speedModifier) % anims[person.animation].length);
-    var anim = anims[person.animation];
+    var frame_num = Math.floor((performance.now() / 100 * speedModifier) % character.animation.length);
+    var anim = character.animation;
     var frame = anim[frame_num % anim.length];
-    ctx.drawImage(img, 32 * frame, 64 * person.facing, 32, 64, person.pos[0], person.pos[1], 32*4, 64*4);
+    ctx.drawImage(img, 32 * frame, 64 * character.facing, 32, 64, character.position.x-16, character.position.y-64, 32*2, 64*2);
   });
 }
 
@@ -81,18 +121,14 @@ function draw()
   canvas.height = Math.floor(rect.height);
 
   var  ctx = canvas.getContext("2d");
-  ctx.fillStyle = "red";
   ctx.fillRect(0,0,canvas.width,canvas.height);
   ctx.resetTransform();
-
-  // ctx.translate(canvas.width/2,);
-
+  ctx.translate(canvas.width/2,canvas.height*(2/3));
+  ctx.scale(2,2);
 
   for(var i = 0; i < WORLD.people.length; ++i){
-    var person = WORLD.people[i];
-    drawCharacter(ctx,person);
-
-    // ctx.fillRect();
+    var character = WORLD.people[i];
+    drawCharacter(ctx,character);
   }
 }
 
