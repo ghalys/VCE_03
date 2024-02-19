e085ea136d430f25db54613c4526ae966813fded
ghalys <asentissielidrissi@gmail.com>
2024-02-19 02:33:40 +0100

    moving by a click

--- public/scripts/world.js
+++ public/scripts/world.js
@@ -14,10 +14,31 @@ function onKeyUp( event ) {
    //mark it as being released
    keys[ event.key ] = false;
 };
-
 document.body.addEventListener("keydown", onKeyDown );
 document.body.addEventListener("keyup", onKeyUp );
 
+
+//here we can store the mouse position and state 
+var mouse_pos = [0,0];
+var mouse_buttons = 0;
+var mouse_clicked = false;
+function onMouse(e){
+  mouse_buttons = e.buttons;
+  // if(mouse_buttons ==1){
+    if(e.type == "mousedown")
+    {
+      var rect = canvas.parentNode.getBoundingClientRect();
+      mouse_pos[0] = (e.clientX - rect.left)/2 - canvas.width/4;
+      mouse_pos[1] = (e.clientY - rect.top )/2 - (canvas.height*(1/3));
+      mouse_clicked = true; // it records the last position when the mouse is clicked    
+    }
+    else if (e.type == "mouseup"){
+     mouse_clicked = false;
+    }
+  // }
+}
+canvas.addEventListener("mousedown", onMouse );
+canvas.addEventListener("mouseup", onMouse );
 
 class Position {
   constructor(x = 0, y = 0) {
@@ -57,7 +78,8 @@ class Character {
       this.username =username,
       this.facing = facing,
       this.position = position,
-      this.animation= animation
+      this.animation= animation,
+      this.onMyWay = false // is True when there is a mouse click and the user should go to somewhere till he arrives
   }
 
   //function for changing face direction
@@ -110,10 +132,38 @@ class Character {
   }
 
   // function which allows us to move to a new point
-  moveTo(newX, newY) {
+  moveTo(newX, newY = this.position.y) {
     this.position.setPosition(newX, newY);
   }
 
+  // walk to the mouse 
+  walkTo(newX,dt){
+
+    if(newX>this.position.x){ //we move to the right
+      if(Math.abs(newX-this.position.x)<=32*dt){ //if we are close
+        var dx = newX-this.position.x;
+        this.moveToRight(dx/32);
+        this.onMyWay = false
+      }
+      else{// if we are far from the destination
+        this.moveToRight(dt)
+      }
+    }
+    else if(newX<this.position.x){ // We move to the left
+      if(Math.abs(newX-this.position.x)<=32*dt){ //if we are close
+        var dx = this.position.x-newX;
+        this.moveToLeft(dx/32);
+        this.onMyWay = false
+      }
+      else{// if we are far from the destination
+        this.moveToLeft(dt)
+      }
+    }
+    else{ //we already arrived
+      this.animatIdle();
+      this.onMyWay = false;
+    }
+  }
   // make the interpolate position with a factor t between the current position and the destination
   interpolatePosition(endPosition, t) {
     this.position = Position.interpolate(this.position, endPosition, t);
@@ -204,7 +254,7 @@ function drawCharacter(ctx, character) {
     var frame_num = Math.floor((performance.now() / 100 * speedModifier) % character.animation.length);
     var anim = character.animation;
     var frame = anim[frame_num % anim.length];
-    ctx.drawImage(img, 32 * frame, 64 * character.facing, 32, 64, character.position.x-16, character.position.y-64, 32*2, 64*2);
+    ctx.drawImage(img, 32 * frame, 64 * character.facing, 32, 64, character.position.x-32, character.position.y-64, 32*2, 64*2);
   });
 }
 
@@ -235,16 +285,25 @@ function update(dt)
 {
   WORLD.myCharacter.animatIdle();
   if(keys["ArrowRight"]){
+    WORLD.myCharacter.onMyWay = false;
     WORLD.myCharacter.moveToRight(dt);
   }
   else if (keys["ArrowLeft"]){
+    WORLD.myCharacter.onMyWay = false;
     WORLD.myCharacter.moveToLeft(dt);
   }
   else if (keys["ArrowDown"]) {
+    WORLD.myCharacter.onMyWay = false;
     WORLD.myCharacter.sitDown();
   }
   else if (keys["ArrowUp"]) {
+    WORLD.myCharacter.onMyWay = false;
     WORLD.myCharacter.standUp();// not needed now 
+  }
+
+  if (WORLD.myCharacter.onMyWay || mouse_clicked){
+    WORLD.myCharacter.onMyWay = true;
+    WORLD.myCharacter.walkTo(mouse_pos[0],dt);
   }
 
 }
@@ -264,3 +323,5 @@ function mainLoop()
 
 
 mainLoop();
+
+//drawARoom
