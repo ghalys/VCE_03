a07b503c674b010baa25cf8ddd8376c0daf4105c
FeliksMarksen <felixmarxen@outlook.com>
2024-02-21 19:59:46 +0100

    Refactor world.js for improved readability and consistency

--- public/scripts/world.js
+++ public/scripts/world.js
@@ -1,7 +1,7 @@
-import {Agent} from './classes.js';
+import { Agent } from "./classes.js";
 
-export class World{
-  constructor(myAgent,canvas){
+export class World {
+  constructor(myAgent, canvas) {
     this.myAgent = myAgent;
     this.peopleById = {};
     this.images = {};
@@ -9,50 +9,46 @@ export class World{
     this.WSserver = null;
   }
 
-  set_ID_and_Server(WSServer){
+  set_ID_and_Server(WSServer) {
     this.myAgent.setId(WSServer.user_id);
     this.WSserver = WSServer;
-
   }
-  initialisation(){
+  initialisation() {
     console.log("tick should start correctly");
     //send the Agent state to the server every 50ms
-    setInterval(this.onTick,1000/20);
+    setInterval(this.onTick, 1000 / 20);
   }
-  
-  onTick= ()=>{
+
+  onTick = () => {
     //Create the Agent state to the server
     var myState = this.myAgent.sendJSON();
     this.WSserver.sendAgentState(myState);
-    // Send it 
+    // Send it
+  };
+
+  leaveTheRoom() {
+    this.peopleById = {};
   }
 
-
-  leaveTheRoom(){
-    this.peopleById ={};
-  }
-
-  addOrUpdateAgent(agentState){
+  addOrUpdateAgent(agentState) {
     var id = agentState.id;
     if (id in this.peopleById) {
       this.peopleById[id].updateFromJSON(agentState);
-    }
-    else{
+    } else {
       //TODO - Maybe we will need more info to get for the first time
-      var agent = new Agent(id,agentState.username);// we have to create a new agent
+      var agent = new Agent(id, agentState.username); // we have to create a new agent
       agent.updateFromJSON(agentState);
       this.peopleById[id] = agent;
     }
-
   }
 
-  removeAgent(agent_state){
+  removeAgent(agent_state) {
     //if the id is present, we remove the agent from myWorld
     if (agent_state.id in this.peopleById) {
       delete this.peopleById[agent_state.id];
     }
   }
-  
+
   //get image
   getImage(url, callback) {
     if (this.images[url]) {
@@ -60,54 +56,68 @@ export class World{
       return;
     }
     var img = new Image();
-    img.onload = ()=> {
+    img.onload = () => {
       this.images[url] = img;
       callback(img);
     };
-    img.onerror = function() {
+    img.onerror = function () {
       console.error("Failed to load image at " + url);
     };
     img.src = url;
   }
 
-  
   drawAgent(ctx, Agent) {
-    this.getImage("Avatar.png", (img)=> {
+    this.getImage("media/Avatar.png", (img) => {
       ctx.imageSmoothingEnabled = false;
-      
+
       // Define a speed modifier
-      var speedModifier = 0.5; 
+      var speedModifier = 0.5;
 
       // Adjust the frame calculation to include the speed modifier
-      var frame_num = Math.floor((performance.now() / 100 * speedModifier) % Agent.animation.length);
+      var frame_num = Math.floor(
+        ((performance.now() / 100) * speedModifier) % Agent.animation.length
+      );
       var anim = Agent.animation;
       var frame = anim[frame_num % anim.length];
-      ctx.drawImage(img, 32 * frame, 64 * Agent.facing, 32, 64, Agent.position.x-32, Agent.position.y-64, 32*2, 64*2);
-      
+      ctx.drawImage(
+        img,
+        32 * frame,
+        64 * Agent.facing,
+        32,
+        64,
+        Agent.position.x - 32,
+        Agent.position.y - 64,
+        32 * 2,
+        64 * 2
+      );
+
       // write the username on the top of the agent
-      ctx.font = "10px Arial"; 
+      ctx.font = "10px Arial";
       ctx.fillStyle = "red";
-      ctx.fillText(Agent.username, Agent.position.x-16, Agent.position.y-64);
-      });
+      ctx.fillText(
+        Agent.username,
+        Agent.position.x - 16,
+        Agent.position.y - 64
+      );
+    });
   }
 
-  draw(){
+  draw() {
     var rect = this.canvas.parentNode.getBoundingClientRect();
     this.canvas.width = Math.floor(rect.width);
     this.canvas.height = Math.floor(rect.height);
 
-    var  ctx = this.canvas.getContext("2d");
-    ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
+    var ctx = this.canvas.getContext("2d");
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     ctx.resetTransform();
-    ctx.translate(this.canvas.width/2,this.canvas.height*(2/3));
-    ctx.scale(2,2);
+    ctx.translate(this.canvas.width / 2, this.canvas.height * (2 / 3));
+    ctx.scale(2, 2);
 
     for (let id in this.peopleById) {
       var Agent = this.peopleById[id];
-      this.drawAgent(ctx,Agent);
+      this.drawAgent(ctx, Agent);
     }
 
-    this.drawAgent(ctx,this.myAgent);
+    this.drawAgent(ctx, this.myAgent);
   }
 }
-
